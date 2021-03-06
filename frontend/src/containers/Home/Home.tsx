7426dc8dac3454395e51a478186192fb8a7e4a2c
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2021-03-06 13:38:30 +0200

    Added user fetch

--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -6,10 +6,12 @@ import {authActions} from "../../reducers/auth/actions";
 import {connect} from "react-redux";
 import tokenService from "../../services/tokenService";
 import {ICurrentUser} from "../../reducers/auth/reducer";
+import LoaderWrapper from "../../components/LoaderWrapper/LoaderWrapper";
 
 interface IPropsFromDispatch {
     actions: {
         removeCurrentUser: typeof authActions.removeCurrentUser;
+        setCurrentUser: typeof authActions.setCurrentUser;
     };
 }
 
@@ -17,11 +19,30 @@ interface IPropsFromState {
     currentUser?: ICurrentUser;
 }
 
+interface IState {
+    loadingUser: boolean;
+}
+
 class Home extends React.Component<RouteComponentProps & IPropsFromDispatch & IPropsFromState> {
+
+    state = {
+        loadingUser: false,
+    } as IState;
+
     logout = () => {
         tokenService.removeTokens();
         this.props.actions.removeCurrentUser();
         this.props.history.push("/auth");
+    }
+
+    componentDidMount() {
+        if (tokenService.isLoggedIn()) {
+            this.setState({loadingUser: true});
+            setTimeout(() => {
+                this.props.actions.setCurrentUser({id: "id"});
+                this.setState({loadingUser: false});
+            }, 1000);
+        }
     }
 
     render() {
@@ -29,11 +50,13 @@ class Home extends React.Component<RouteComponentProps & IPropsFromDispatch & IP
             return <Redirect to="/auth" />;
         }
 
+        const {loadingUser} = this.state;
+
         return (
-            <div>
+            <LoaderWrapper loading={loadingUser}>
                 Home
                 <button onClick={this.logout}>Logout</button>
-            </div>
+            </LoaderWrapper>
         );
     }
 }
@@ -46,10 +69,12 @@ const mapDispatchToProps = (dispatch: any) => ({
     actions:
         bindActionCreators<any,
             {
-                removeCurrentUser: typeof authActions.removeCurrentUser
+                removeCurrentUser: typeof authActions.removeCurrentUser,
+                setCurrentUser: typeof authActions.setCurrentUser,
             }>(
             {
-                removeCurrentUser: authActions.removeCurrentUser
+                removeCurrentUser: authActions.removeCurrentUser,
+                setCurrentUser: authActions.setCurrentUser,
             }, dispatch),
 });
 

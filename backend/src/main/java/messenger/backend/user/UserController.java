fc94f14206604f337196935c201121bc4e9fe096
hronograf <vlad.boholii@gmail.com>
2021-03-22 23:15:59 +0200

    add search by username

--- backend/src/main/java/messenger/backend/user/UserController.java
+++ backend/src/main/java/messenger/backend/user/UserController.java
@@ -1,18 +1,27 @@
 package messenger.backend.user;
 
 import lombok.RequiredArgsConstructor;
+import messenger.backend.user.dto.UserSearchInfoDto;
+import messenger.backend.utils.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
+
 @RestController
 @RequestMapping("/api/users")
-@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/search") //todo @NotEmpty validation
+    public Response<UserSearchInfoDto> getUserSearchInfo(@RequestParam(name = "username") String username) {
+        return Response.success(userService.getUserSearchInfo(username));
+    }
 
     @GetMapping("/all") //todo delete this
     public List<UserEntity> getAllUsers() {
--- backend/src/main/java/messenger/backend/user/UserService.java
+++ backend/src/main/java/messenger/backend/user/UserService.java
@@ -1,15 +1,24 @@
 package messenger.backend.user;
 
 import lombok.RequiredArgsConstructor;
+import messenger.backend.user.dto.UserSearchInfoDto;
+import messenger.backend.user.exceptions.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 @RequiredArgsConstructor
+
+@Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserSearchInfoDto getUserSearchInfo(String username) {
+        UserEntity userEntity = userRepository.getByUsername(username)
+                .orElseThrow(UserNotFoundException::new);
+        return UserSearchInfoDto.from(userEntity);
+    }
 
     // TODO delete
     public List<UserEntity> getAllUsers() {

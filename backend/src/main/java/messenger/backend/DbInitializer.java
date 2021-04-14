f5e4666b57f048e0abeadba464c11b6f3be6a1bf
LordNani <rigansu@gmail.com>
2021-04-14 22:14:34 +0300

    Update DbInitializer.java

--- backend/src/main/java/messenger/backend/DbInitializer.java
+++ backend/src/main/java/messenger/backend/DbInitializer.java
@@ -19,6 +19,8 @@ public class DbInitializer {
     @EventListener
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
+
+        
         fakerService.generateRandomData();
     }
 }

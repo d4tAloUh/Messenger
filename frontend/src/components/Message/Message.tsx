eea87f0b73255a1bd9d61e0ec448d6f097ab6c8d
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2021-04-16 17:44:31 +0300

    Fixed chat read on send, own name display

--- frontend/src/components/Message/Message.tsx
+++ frontend/src/components/Message/Message.tsx
@@ -7,18 +7,19 @@ import moment from "moment";
 interface IOwnProps {
     message: IMessageWrapper;
     isVisibleName?: boolean;
+    ownMessage: boolean;
 }
 
 class Message extends React.Component<IOwnProps> {
     render() {
-        const {message, isVisibleName} = this.props;
+        const {message, isVisibleName, ownMessage} = this.props;
         const classes = classnames(styles.message, message.loading && styles.loading);
         const text = message.info?.text || message.loading?.text;
         const momentCreatedAt = moment(message.info?.createdAt);
 
         return (
             <div className={classes}>
-                {isVisibleName && (
+                {isVisibleName && !ownMessage && (
                     <div className={styles.name}>
                         {message.info?.senderName}
                     </div>
